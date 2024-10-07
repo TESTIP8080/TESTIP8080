c73a11984596e5443b1c72db7bd7a2cb1279c871
TESTIP8080 <lmo.markets@example.com>
2024-10-07 12:48:12 +0600

    Updates

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const CACHE_NAME="ao-analyzer-v1",urlsToCache=["/","/index.html","/bundle.js","/manifest.json","/favicon.ico","/logo192.png","/logo512.png"];self.addEventListener("install",(e=>{e.waitUntil(caches.open(CACHE_NAME).then((e=>e.addAll(urlsToCache))))})),self.addEventListener("fetch",(e=>{e.respondWith(caches.match(e.request).then((n=>n||fetch(e.request))))}));
